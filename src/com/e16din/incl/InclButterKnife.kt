e10948e133bee04ba2c7ef9772018920719c0843
e16din <bigusov@gmail.com>
2016-12-10 03:03:51 +0300

    fix InclButterKnife issues; fix InclDBFlow issue;

--- src/com/e16din/incl/InclButterKnife.kt
+++ src/com/e16din/incl/InclButterKnife.kt
@@ -5,12 +5,13 @@ import com.e16din.incl.BaseIncl.InsertionType.*
 class InclButterKnife : BaseIncl() {
 
     companion object {
-        const val CLASSPATH_BUTTERKNIFE = "classpath 'com.jakewharton:butterknife-gradle-plugin:1.8'"
+        const val VER = "8.4.0"
+        const val CLASSPATH_BUTTERKNIFE = "classpath 'com.jakewharton:butterknife-gradle-plugin:$VER'"
 
         const val PLUGIN_BUTTERKNIFE = "com.jakewharton.butterknife"
 
-        const val VERSION_BUTTERKNIFE = """def ver_butterkinife = "8.4.0""""
-        const val COMPILE_BUTTERKNIFE = """"compile "com.jakewharton:butterknife:${'$'}{ver_butterkinife}""""
+        const val VERSION_BUTTERKNIFE = """def ver_butterkinife = "$VER""""
+        const val COMPILE_BUTTERKNIFE = """compile "com.jakewharton:butterknife:${'$'}{ver_butterkinife}""""
         const val APT_BUTTERKNIFE = """apt "com.jakewharton:butterknife-compiler:${'$'}{ver_butterkinife}""""
     }
 
@@ -25,7 +26,7 @@ class InclButterKnife : BaseIncl() {
         insertToGradleBlock(GRADLE_BLOCK_DEPENDENCIES, COMPILE_BUTTERKNIFE)
         insertToGradleBlock(GRADLE_BLOCK_DEPENDENCIES, APT_BUTTERKNIFE)
 
-        insert(TYPE_ALL_PROJECTS_REPOSITORY, "\n        ${REPOSITORY_MAVEN_CENTRAL}")
+        insert(TYPE_ALL_PROJECTS_REPOSITORY, "\n        $REPOSITORY_MAVEN_CENTRAL")
         insert(TYPE_DEPENDENCIES_CLASSPATH, CLASSPATH_BUTTERKNIFE)
     }
 
--- src/com/e16din/incl/InclDBFlow.kt
+++ src/com/e16din/incl/InclDBFlow.kt
@@ -9,7 +9,7 @@ class InclDBFlow : BaseIncl() {
         const val COMPILE_DBFLOW_CORE = """compile "com.github.Raizlabs.DBFlow:dbflow-core:${'$'}{ver_dbflow}""""
         const val COMPILE_DBFLOW = """compile "com.github.Raizlabs.DBFlow:dbflow:${'$'}{ver_dbflow}""""
 
-        const val COMPILE_DBFLOW_KOTLIN = """com.github.Raizlabs.DBFlow:dbflow-kotlinextensions:${'$'}{sqlcipher_version}@aar""""
+        const val COMPILE_DBFLOW_KOTLIN = """compile "com.github.Raizlabs.DBFlow:dbflow-kotlinextensions:${'$'}{ver_dbflow}@aar""""
         const val COMPILE_DBFLOW_SQLCIPHER = """compile "com.github.Raizlabs.DBFlow:dbflow-sqlcipher:${'$'}{ver_dbflow}""""
         const val COMPILE_ZETERIC_SQLCIPHER = """compile "net.zetetic:android-database-sqlcipher:${'$'}{sqlcipher_version}@aar""""
     }
@@ -22,12 +22,20 @@ class InclDBFlow : BaseIncl() {
         includeJitpack()
 
 
+        val prefix = "\n    "
         val dependencies = arrayOf("", VERSION_DBFLOW, APT_DBFLOW_PROCESSOR, COMPILE_DBFLOW_CORE, COMPILE_DBFLOW)
-                .joinToString("\n    ")
+                .joinToString(prefix)
         insertToGradleBlock(GRADLE_BLOCK_DEPENDENCIES, dependencies)
 
-        val commentedDependencies = arrayOf("(optional)", COMPILE_DBFLOW_KOTLIN, COMPILE_DBFLOW_SQLCIPHER, COMPILE_ZETERIC_SQLCIPHER)
-                .joinToString("\n    ")
+        val commentedDependencies = arrayOf("(optional)",
+                COMPILE_DBFLOW_KOTLIN, COMPILE_DBFLOW_SQLCIPHER, COMPILE_ZETERIC_SQLCIPHER).joinToString(prefix)
         insertToGradleBlock(GRADLE_BLOCK_DEPENDENCIES, commentedDependencies, commented = true)
+
+        insertToClass(ClassType.TYPE_APPLICATION, BlockType.BLOCK_ON_CREATE, "\n        FlowManager.init(FlowConfig.Builder(this).build());")
+        insertToClass(ClassType.TYPE_APPLICATION, BlockType.BLOCK_ON_CREATE, "\n        FlowLog.setMinimumLoggingLevel(FlowLog.Level.W);")
+
+        insertToClass(ClassType.TYPE_APPLICATION, BlockType.BLOCK_IMPORT, "\nimport com.raizlabs.android.dbflow.config.FlowConfig;")
+        insertToClass(ClassType.TYPE_APPLICATION, BlockType.BLOCK_IMPORT, "\nimport com.raizlabs.android.dbflow.config.FlowLog;")
+        insertToClass(ClassType.TYPE_APPLICATION, BlockType.BLOCK_IMPORT, "\nimport com.raizlabs.android.dbflow.config.FlowManager;")
     }
 }
